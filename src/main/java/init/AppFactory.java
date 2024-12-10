e3fdf96db34c8170556bb375716ee4e61ac75bd8
seymenas <seymen2003@web.de>
2024-12-10 14:31:01 +0100

    AppBuilder und AppFactory implementiert

---
+++ src/main/java/init/AppFactory.java
@@ -0,0 +1,29 @@
+package init;
+
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class AppFactory {
+    private static Repository _REPO;
+    private static EntityManagerFactory _EMF;
+
+    private static Controller _CONTROLLER;
+
+    static Repository createRepository() {
+        if (_REPO == null) {
+            _REPO = new Repository();
+        }
+
+        if (_EMF == null) {
+            _EMF = Persistence.createEntityManagerFactory("hr");
+        }
+        return _REPO;
+    }
+    static Controller createController(Repository repo){
+        if (_CONTROLLER == null) {
+            _CONTROLLER = new Controller(repo);
+        }
+        return _CONTROLLER;
+    }
+}
+
